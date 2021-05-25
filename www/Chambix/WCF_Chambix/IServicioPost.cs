38a09d9ef1945e1ab30ccc9f96331feabdbef3ee
Diego Cáceres-Cardoza <diegokaiser@gmail.com>
2021-05-24 23:27:56 -0500

    parcialServicioPost, falta probar

---
+++ www/Chambix/WCF_Chambix/IServicioPost.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_Chambix
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IServicioPost" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IServicioPost
+    {
+        [OperationContract]
+        Boolean InsertPost(PostBE objPostBE);
+
+        [OperationContract]
+        Boolean UpdatePost(PostBE objPostBE);
+
+        [OperationContract]
+        Boolean LogicDeletePost(PostBE objPostBE);
+
+        [OperationContract]
+        PostBE GetPost(Int16 idPost);
+
+        [OperationContract]
+        List<PostBE> GetAllPosts();
+
+        [OperationContract]
+        List<PostBE> GetAllPostsPorCategoria(Int16 idSubCategoria);
+
+        [OperationContract]
+        Int16 GetContarPostPorCategoria(Int16 idPost);
+    }
+    [DataContract]
+    [Serializable]
+    public class PostBE
+    {
+        private Int16 mvaridPost;
+        private Int16 mvaridUsuario;
+        private Int16 mvaridSubcategoria;
+        private Int16 mvaridDistrito;
+        private String mvartituloPost;
+        private String mvardescripcionPost;
+        private Decimal mvarprecioPost;
+        private String mvarimagenPost;
+        private Int16 mvarvaloracionPost;
+        private Int16 mvarestadoPost;
+        private DateTime mvarcreate_at;
+        private String mvarcreate_by;
+        private DateTime mvarupdate_at;
+        private String mvarupdate_by;
+
+        [DataMember]
+        public Int16 idPost
+        {
+            get { return mvaridPost; }
+            set { mvaridPost = value; }
+        }
+
+        [DataMember]
+        public Int16 idUsuario
+        {
+            get { return mvaridUsuario; }
+            set { mvaridUsuario = value; }
+        }
+
+        [DataMember]
+        public Int16 idSubcategoria
+        {
+            get { return mvaridSubcategoria; }
+            set { mvaridSubcategoria = value; }
+        }
+
+        [DataMember]
+        public Int16 idDistrito
+        {
+            get { return mvaridDistrito; }
+            set { mvaridDistrito = value; }
+        }
+
+        [DataMember]
+        public String tituloPost
+        {
+            get { return mvartituloPost; }
+            set { mvartituloPost = value; }
+        }
+
+        [DataMember]
+        public String descripcionPost
+        {
+            get { return mvardescripcionPost; }
+            set { mvardescripcionPost = value; }
+        }
+
+        [DataMember]
+        public Decimal precioPost
+        {
+            get { return mvarprecioPost; }
+            set { mvarprecioPost = value; }
+        }
+
+        [DataMember]
+        public String imagenPost
+        {
+            get { return mvarimagenPost; }
+            set { mvarimagenPost = value; }
+        }
+
+        [DataMember]
+        public Int16 valoracionPost
+        {
+            get { return mvarvaloracionPost; }
+            set { mvarvaloracionPost = value; }
+        }
+
+        [DataMember]
+        public Int16 estadoPost
+        {
+            get { return mvarestadoPost; }
+            set { mvarestadoPost = value; }
+        }
+
+        [DataMember]
+        public DateTime create_at
+        {
+            get { return mvarcreate_at; }
+            set { mvarcreate_at = value; }
+        }
+
+        [DataMember]
+        public String create_by
+        {
+            get { return mvarcreate_by; }
+            set { mvarcreate_by = value; }
+        }
+        
+        [DataMember]
+        public DateTime update_at
+        {
+            get { return mvarupdate_at; }
+            set { mvarupdate_at = value; }
+        }
+
+        [DataMember]
+        public String update_by
+        {
+            get { return mvarupdate_by; }
+            set { mvarupdate_by = value; }
+        }
+    }
+}
---
+++ www/Chambix/WCF_Chambix/ServicioPost.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data.Entity.Core;
+
+namespace WCF_Chambix
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "ServicioPost" en el código y en el archivo de configuración a la vez.
+    public class ServicioPost : IServicioPost
+    {
+        public Boolean InsertPost(PostBE objPostBE)
+        {
+            SistemaServiciosEntities Chambix = new SistemaServiciosEntities();
+            try
+            {
+                tb_Post objPost = new tb_Post();
+                objPost.idPost          = 0;
+                objPost.idUsuario       = objPostBE.idUsuario;
+                objPost.idSubcategoria  = objPostBE.idSubcategoria;
+                objPost.idDistrito      = Convert.ToByte(objPostBE.idDistrito);
+                objPost.tituloPost      = objPostBE.tituloPost;
+                objPost.descripcionPost = objPostBE.descripcionPost;
+                objPost.precioPost      = Convert.ToDecimal(objPostBE.precioPost);
+                objPost.imagenPost      = objPostBE.imagenPost;
+                objPost.valoracionPost  = objPostBE.valoracionPost;
+                objPost.estadoPost      = Convert.ToBoolean(objPostBE.estadoPost);
+                objPost.create_at       = Convert.ToDateTime(objPostBE.create_at);
+                objPost.create_by       = objPostBE.create_by;
+                objPost.update_at       = Convert.ToDateTime(objPostBE.update_at);
+                objPost.update_by       = objPostBE.update_by;
+                Chambix.tb_Post.Add(objPost);
+                Chambix.SaveChanges();
+                return true;
+            }
+            catch (EntityException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Boolean UpdatePost(PostBE objPostBE)
+        {
+            SistemaServiciosEntities Chambix = new SistemaServiciosEntities();
+            try
+            {
+                tb_Post objPost = (from objPst in Chambix.tb_Post
+                                   where objPst.idPost == objPostBE.idPost
+                                   select objPst).FirstOrDefault();
+                objPost.idPost          = objPostBE.idPost;
+                objPost.idUsuario       = objPostBE.idUsuario;
+                objPost.idSubcategoria  = objPostBE.idSubcategoria;
+                objPost.idDistrito      = Convert.ToByte(objPostBE.idDistrito);
+                objPost.tituloPost      = objPostBE.tituloPost;
+                objPost.descripcionPost = objPostBE.descripcionPost;
+                objPost.precioPost      = Convert.ToDecimal(objPostBE.precioPost);
+                objPost.imagenPost      = objPostBE.imagenPost;
+                objPost.valoracionPost  = objPostBE.valoracionPost;
+                objPost.estadoPost      = Convert.ToBoolean(objPostBE.estadoPost);
+                objPost.create_at       = Convert.ToDateTime(objPostBE.create_at);
+                objPost.create_by       = objPostBE.create_by;
+                objPost.update_at       = Convert.ToDateTime(objPostBE.update_at);
+                objPost.update_by       = objPostBE.update_by;
+                Chambix.SaveChanges();
+                return true;
+            }
+            catch (EntityException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Boolean LogicDeletePost(PostBE objPostBE)
+        {
+            SistemaServiciosEntities Chambix = new SistemaServiciosEntities();
+            try
+            {
+                tb_Post objPost = (from objPst in Chambix.tb_Post
+                                   where objPst.idPost == objPostBE.idPost
+                                   select objPst).FirstOrDefault();
+                objPost.estadoPost = false;
+                Chambix.SaveChanges();
+                return true;
+            }
+            catch (EntityException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public PostBE GetPost(Int16 idPost)
+        {
+            SistemaServiciosEntities Chambix = new SistemaServiciosEntities();
+            try
+            {
+                tb_Post objPost = (from objPst in Chambix.tb_Post
+                                   where objPst.idPost == idPost
+                                   && objPst.estadoPost == true
+                                   select objPst).FirstOrDefault();
+                PostBE objPostBE          = new PostBE();
+                objPostBE.idPost          = Convert.ToInt16(objPost.idPost);
+                objPostBE.idUsuario       = Convert.ToInt16(objPost.idUsuario);
+                objPostBE.idSubcategoria  = Convert.ToInt16(objPost.idSubcategoria);
+                objPostBE.idDistrito      = Convert.ToInt16(objPost.idDistrito);
+                objPostBE.tituloPost      = objPost.tituloPost;
+                objPostBE.descripcionPost = objPost.descripcionPost;
+                objPostBE.imagenPost      = objPost.imagenPost;
+                objPostBE.precioPost      = Convert.ToDecimal(objPost.precioPost);
+                objPostBE.valoracionPost  = Convert.ToInt16(objPost.valoracionPost);
+                objPostBE.estadoPost      = Convert.ToInt16(objPost.estadoPost);
+                objPostBE.create_at       = objPost.create_at;
+                return objPostBE;
+            }
+            catch (EntityException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public List<PostBE> GetAllPosts()
+        {
+            SistemaServiciosEntities Chambix = new SistemaServiciosEntities();
+            try
+            {
+                List<PostBE> objPostLists = new List<PostBE>();
+                var query = (from objPst in Chambix.tb_Post
+                             where objPst.estadoPost == true
+                             select objPst);
+                foreach(var objPost in query)
+                {
+                    PostBE objPostBE = new PostBE();
+                    objPostBE.idPost = Convert.ToInt16(objPost.idPost);
+                    objPostBE.idUsuario = Convert.ToInt16(objPost.idUsuario);
+                    objPostBE.idSubcategoria = Convert.ToInt16(objPost.idSubcategoria);
+                    objPostBE.idDistrito = Convert.ToInt16(objPost.idDistrito);
+                    objPostBE.tituloPost = objPost.tituloPost;
+                    objPostBE.descripcionPost = objPost.descripcionPost;
+                    objPostBE.imagenPost = objPost.imagenPost;
+                    objPostBE.precioPost = Convert.ToDecimal(objPost.precioPost);
+                    objPostBE.valoracionPost = Convert.ToInt16(objPost.valoracionPost);
+                    objPostBE.estadoPost = Convert.ToInt16(objPost.estadoPost);
+                    objPostBE.create_at = objPost.create_at;
+                    objPostLists.Add(objPostBE);
+                }
+                return objPostLists;
+            }
+            catch (EntityException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public List<PostBE> GetAllPostsPorCategoria(Int16 idSubCategoria)
+        {
+            SistemaServiciosEntities Chambix = new SistemaServiciosEntities();
+            try
+            {
+                List<PostBE> objPostLists = new List<PostBE>();
+                var query = (from objPst in Chambix.tb_Post
+                             where objPst.estadoPost == true
+                             && objPst.idSubcategoria == idSubCategoria
+                             select objPst);
+                foreach (var objPost in query)
+                {
+                    PostBE objPostBE = new PostBE();
+                    objPostBE.idPost = Convert.ToInt16(objPost.idPost);
+                    objPostBE.idUsuario = Convert.ToInt16(objPost.idUsuario);
+                    objPostBE.idSubcategoria = Convert.ToInt16(objPost.idSubcategoria);
+                    objPostBE.idDistrito = Convert.ToInt16(objPost.idDistrito);
+                    objPostBE.tituloPost = objPost.tituloPost;
+                    objPostBE.descripcionPost = objPost.descripcionPost;
+                    objPostBE.imagenPost = objPost.imagenPost;
+                    objPostBE.precioPost = Convert.ToDecimal(objPost.precioPost);
+                    objPostBE.valoracionPost = Convert.ToInt16(objPost.valoracionPost);
+                    objPostBE.estadoPost = Convert.ToInt16(objPost.estadoPost);
+                    objPostBE.create_at = objPost.create_at;
+                    objPostLists.Add(objPostBE);
+                }
+                return objPostLists;
+            }
+            catch (EntityException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Int16 GetContarPostPorCategoria(Int16 idPost)
+        {
+            SistemaServiciosEntities Chambix = new SistemaServiciosEntities();
+            try
+            {
+                Int16 cantPostPorCategoria = Convert.ToInt16(
+                    (from objPst in Chambix.tb_Post
+                     where objPst.idPost == idPost
+                     && objPst.estadoPost == true
+                     select objPst).Count()
+                    );
+                return cantPostPorCategoria;
+            }
+            catch (EntityException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
