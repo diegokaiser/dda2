bd0ee3560465491c413032ef006855264fd4ff49
Diego Cáceres-Cardoza <diegokaiser@gmail.com>
2021-05-27 00:34:12 -0500

    parcialServicioUsuario, falta probar

---
+++ www/Chambix/WCF_Chambix/ServicioUsuario.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data.Entity.Core;
+
+namespace WCF_Chambix
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "ServicioUsuario" en el código y en el archivo de configuración a la vez.
+    public class ServicioUsuario : IServicioUsuario
+    {
+        public Boolean InsertUser(UserBE objUserBE)
+        {
+            SistemaServiciosEntities Chambix = new SistemaServiciosEntities();
+            try
+            {
+                tb_Usuario objUser = new tb_Usuario();
+                objUser.idUsuario         = 0;
+                objUser.tipoUsuario       = Convert.ToByte(objUserBE.tipoUsuario);
+                objUser.estadoUsuario     = Convert.ToBoolean(objUserBE.estadoUsuario);
+                objUser.nombreUsuario     = objUserBE.nombreUsuario;
+                objUser.apellidoUsuario   = objUserBE.apellidoUsuario;
+                objUser.contrasenaUsuario = objUserBE.contrasenaUsuario;
+                objUser.idDistrito        = Convert.ToByte(objUserBE.idDistrito);
+                objUser.emailUsuario      = objUserBE.emailUsuario;
+                objUser.cellUsuario       = objUserBE.cellUsuario;
+                objUser.rankUsuario       = objUserBE.rankUsuario;
+                objUser.wspUsuario        = objUserBE.wspUsuario;
+                objUser.ocupacionUsuario  = objUserBE.ocupacionUsuario;
+                objUser.idTipoCuenta      = Convert.ToByte(objUserBE.idTipoCuenta);
+                objUser.tiempoCuenta      = Convert.ToDateTime(objUserBE.tiempoCuenta);
+                objUser.create_at         = Convert.ToDateTime(objUserBE.create_at);
+                objUser.create_by         = objUserBE.create_by;
+                objUser.update_at         = Convert.ToDateTime(objUserBE.update_at);
+                objUser.update_by         = objUserBE.update_by;
+                Chambix.tb_Usuario.Add(objUser);
+                Chambix.SaveChanges();
+                return true;
+            }
+            catch (EntityException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Boolean UpdatePost(UserBE objUserBE)
+        {
+            SistemaServiciosEntities Chambix = new SistemaServiciosEntities();
+            try
+            {
+                tb_Usuario objUser = (from objUsr in Chambix.tb_Usuario
+                                      where objUsr.idUsuario == objUserBE.idUsuario
+                                      select objUsr).FirstOrDefault();
+                objUser.idUsuario         = objUserBE.idUsuario;
+                objUser.tipoUsuario       = Convert.ToByte(objUserBE.tipoUsuario);
+                objUser.estadoUsuario     = Convert.ToBoolean(objUserBE.estadoUsuario);
+                objUser.nombreUsuario     = objUserBE.nombreUsuario;
+                objUser.apellidoUsuario   = objUserBE.apellidoUsuario;
+                objUser.contrasenaUsuario = objUserBE.contrasenaUsuario;
+                objUser.idDistrito        = Convert.ToByte(objUserBE.idDistrito);
+                objUser.emailUsuario      = objUserBE.emailUsuario;
+                objUser.cellUsuario       = objUserBE.cellUsuario;
+                objUser.rankUsuario       = objUserBE.rankUsuario;
+                objUser.wspUsuario        = objUserBE.wspUsuario;
+                objUser.ocupacionUsuario  = objUserBE.ocupacionUsuario;
+                objUser.idTipoCuenta      = Convert.ToByte(objUserBE.idTipoCuenta);
+                objUser.tiempoCuenta      = Convert.ToDateTime(objUserBE.tiempoCuenta);
+                objUser.create_at         = Convert.ToDateTime(objUserBE.create_at);
+                objUser.create_by         = objUserBE.create_by;
+                objUser.update_at         = Convert.ToDateTime(objUserBE.update_at);
+                objUser.update_by         = objUserBE.update_by;
+                Chambix.SaveChanges();
+                return true;
+            }
+            catch (EntityException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Boolean LogicDeletePost(UserBE objUserBE)
+        {
+            SistemaServiciosEntities Chambix = new SistemaServiciosEntities();
+            try
+            {
+                tb_Usuario objUser = (from objUsr in Chambix.tb_Usuario
+                                      where objUsr.idUsuario == objUserBE.idUsuario
+                                      select objUsr).FirstOrDefault();
+                objUser.estadoUsuario = false;
+                Chambix.SaveChanges();
+                return true;
+            }
+            catch (EntityException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public UserBE GetUser(Int16 idUser)
+        {
+            SistemaServiciosEntities Chambix = new SistemaServiciosEntities();
+            try
+            {
+                tb_Usuario objUser = (from objUsr in Chambix.tb_Usuario
+                                      where objUsr.idUsuario == idUser
+                                      && objUsr.estadoUsuario == true
+                                      select objUsr).FirstOrDefault();
+                UserBE objUserBE            = new UserBE();
+                objUserBE.idUsuario         = Convert.ToInt16(objUser.idUsuario);
+                objUserBE.tipoUsuario       = Convert.ToInt16(objUser.tipoUsuario);
+                objUserBE.estadoUsuario     = Convert.ToInt16(objUser.estadoUsuario);
+                objUserBE.nombreUsuario     = objUser.nombreUsuario;
+                objUserBE.apellidoUsuario   = objUser.apellidoUsuario;
+                objUserBE.contrasenaUsuario = objUser.contrasenaUsuario;
+                objUserBE.idDistrito        = Convert.ToInt16(objUser.idDistrito);
+                objUserBE.emailUsuario      = objUser.emailUsuario;
+                objUserBE.cellUsuario       = objUser.cellUsuario;
+                objUserBE.rankUsuario       = Convert.ToInt16(objUser.rankUsuario);
+                objUserBE.wspUsuario        = objUser.wspUsuario;
+                objUserBE.ocupacionUsuario  = objUser.ocupacionUsuario;
+                objUserBE.idTipoCuenta      = objUser.idTipoCuenta;
+                objUserBE.tiempoCuenta      = Convert.ToDateTime(objUser.tiempoCuenta);
+                objUserBE.create_at         = objUser.create_at;
+                return objUserBE;
+            }
+            catch (EntityException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public List<UserBE> GetAllUsers()
+        {
+            SistemaServiciosEntities Chambix = new SistemaServiciosEntities();
+            try
+            {
+                List<UserBE> objUserLists = new List<UserBE>();
+                var query = (from objUsr in Chambix.tb_Usuario
+                             where objUsr.estadoUsuario == true
+                             select objUsr);
+                foreach(var objUser in query)
+                {
+                    UserBE objUserBE = new UserBE();
+                    objUserBE.idUsuario = Convert.ToInt16(objUser.idUsuario);
+                    objUserBE.tipoUsuario = Convert.ToInt16(objUser.tipoUsuario);
+                    objUserBE.estadoUsuario = Convert.ToInt16(objUser.estadoUsuario);
+                    objUserBE.nombreUsuario = objUser.nombreUsuario;
+                    objUserBE.apellidoUsuario = objUser.apellidoUsuario;
+                    objUserBE.contrasenaUsuario = objUser.contrasenaUsuario;
+                    objUserBE.idDistrito = Convert.ToInt16(objUser.idDistrito);
+                    objUserBE.emailUsuario = objUser.emailUsuario;
+                    objUserBE.cellUsuario = objUser.cellUsuario;
+                    objUserBE.rankUsuario = Convert.ToInt16(objUser.rankUsuario);
+                    objUserBE.wspUsuario = objUser.wspUsuario;
+                    objUserBE.ocupacionUsuario = objUser.ocupacionUsuario;
+                    objUserBE.idTipoCuenta = objUser.idTipoCuenta;
+                    objUserBE.tiempoCuenta = Convert.ToDateTime(objUser.tiempoCuenta);
+                    objUserBE.create_at = objUser.create_at;
+                }
+                return objUserLists;
+            }
+            catch (EntityException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public List<UserBE> GetAllUsersClients(Int16 tipoUsuario)
+        {
+            SistemaServiciosEntities Chambix = new SistemaServiciosEntities();
+            try
+            {
+                List<UserBE> objUserLists = new List<UserBE>();
+                var query = (from objUsr in Chambix.tb_Usuario
+                             where objUsr.estadoUsuario == true
+                             && objUsr.tipoUsuario == 1
+                             select objUsr);
+                foreach (var objUser in query)
+                {
+                    UserBE objUserBE = new UserBE();
+                    objUserBE.idUsuario = Convert.ToInt16(objUser.idUsuario);
+                    objUserBE.tipoUsuario = Convert.ToInt16(objUser.tipoUsuario);
+                    objUserBE.estadoUsuario = Convert.ToInt16(objUser.estadoUsuario);
+                    objUserBE.nombreUsuario = objUser.nombreUsuario;
+                    objUserBE.apellidoUsuario = objUser.apellidoUsuario;
+                    objUserBE.contrasenaUsuario = objUser.contrasenaUsuario;
+                    objUserBE.idDistrito = Convert.ToInt16(objUser.idDistrito);
+                    objUserBE.emailUsuario = objUser.emailUsuario;
+                    objUserBE.cellUsuario = objUser.cellUsuario;
+                    objUserBE.rankUsuario = Convert.ToInt16(objUser.rankUsuario);
+                    objUserBE.wspUsuario = objUser.wspUsuario;
+                    objUserBE.ocupacionUsuario = objUser.ocupacionUsuario;
+                    objUserBE.idTipoCuenta = objUser.idTipoCuenta;
+                    objUserBE.tiempoCuenta = Convert.ToDateTime(objUser.tiempoCuenta);
+                    objUserBE.create_at = objUser.create_at;
+                }
+                return objUserLists;
+            }
+            catch (EntityException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public List<UserBE> GetAllUsersProfesionals(Int16 tipoUsuario)
+        {
+            SistemaServiciosEntities Chambix = new SistemaServiciosEntities();
+            try
+            {
+                List<UserBE> objUserLists = new List<UserBE>();
+                var query = (from objUsr in Chambix.tb_Usuario
+                             where objUsr.estadoUsuario == true
+                             && objUsr.tipoUsuario == 2
+                             select objUsr);
+                foreach (var objUser in query)
+                {
+                    UserBE objUserBE = new UserBE();
+                    objUserBE.idUsuario = Convert.ToInt16(objUser.idUsuario);
+                    objUserBE.tipoUsuario = Convert.ToInt16(objUser.tipoUsuario);
+                    objUserBE.estadoUsuario = Convert.ToInt16(objUser.estadoUsuario);
+                    objUserBE.nombreUsuario = objUser.nombreUsuario;
+                    objUserBE.apellidoUsuario = objUser.apellidoUsuario;
+                    objUserBE.contrasenaUsuario = objUser.contrasenaUsuario;
+                    objUserBE.idDistrito = Convert.ToInt16(objUser.idDistrito);
+                    objUserBE.emailUsuario = objUser.emailUsuario;
+                    objUserBE.cellUsuario = objUser.cellUsuario;
+                    objUserBE.rankUsuario = Convert.ToInt16(objUser.rankUsuario);
+                    objUserBE.wspUsuario = objUser.wspUsuario;
+                    objUserBE.ocupacionUsuario = objUser.ocupacionUsuario;
+                    objUserBE.idTipoCuenta = objUser.idTipoCuenta;
+                    objUserBE.tiempoCuenta = Convert.ToDateTime(objUser.tiempoCuenta);
+                    objUserBE.create_at = objUser.create_at;
+                }
+                return objUserLists;
+            }
+            catch (EntityException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public List<UserBE> GetAllUsersAdmins(Int16 tipoUsuario)
+        {
+            SistemaServiciosEntities Chambix = new SistemaServiciosEntities();
+            try
+            {
+                List<UserBE> objUserLists = new List<UserBE>();
+                var query = (from objUsr in Chambix.tb_Usuario
+                             where objUsr.estadoUsuario == true
+                             && objUsr.tipoUsuario == 3
+                             select objUsr);
+                foreach (var objUser in query)
+                {
+                    UserBE objUserBE = new UserBE();
+                    objUserBE.idUsuario = Convert.ToInt16(objUser.idUsuario);
+                    objUserBE.tipoUsuario = Convert.ToInt16(objUser.tipoUsuario);
+                    objUserBE.estadoUsuario = Convert.ToInt16(objUser.estadoUsuario);
+                    objUserBE.nombreUsuario = objUser.nombreUsuario;
+                    objUserBE.apellidoUsuario = objUser.apellidoUsuario;
+                    objUserBE.contrasenaUsuario = objUser.contrasenaUsuario;
+                    objUserBE.idDistrito = Convert.ToInt16(objUser.idDistrito);
+                    objUserBE.emailUsuario = objUser.emailUsuario;
+                    objUserBE.cellUsuario = objUser.cellUsuario;
+                    objUserBE.rankUsuario = Convert.ToInt16(objUser.rankUsuario);
+                    objUserBE.wspUsuario = objUser.wspUsuario;
+                    objUserBE.ocupacionUsuario = objUser.ocupacionUsuario;
+                    objUserBE.idTipoCuenta = objUser.idTipoCuenta;
+                    objUserBE.tiempoCuenta = Convert.ToDateTime(objUser.tiempoCuenta);
+                    objUserBE.create_at = objUser.create_at;
+                }
+                return objUserLists;
+            }
+            catch (EntityException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
