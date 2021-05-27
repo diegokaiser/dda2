f621cf1444f848080f355ea65649e32a039c9879
Josue <yoshuai_i@hotmail.com>
2021-05-26 20:55:39 -0500

    Lo de diego mas lo de josue

---
+++ www/Chambix/WCF_Chambix/ServicioDistrito.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_Chambix
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "ServicioDistrito" en el código y en el archivo de configuración a la vez.
+    public class ServicioDistrito : IServicioDistrito
+    {
+        public void DoWork()
+        {
+        }
+
+
+
+        public List<DistritoBE> GetAllDistritos()
+        {
+            SistemaServiciosEntities MisVentas = new SistemaServiciosEntities();
+
+            try
+            {
+                List<DistritoBE> objListaDistritos = new List<DistritoBE>();
+                var query = (from objDis in MisVentas.tb_Distrito
+                             select objDis);
+                foreach (var objVendedor in query)
+                {
+                    DistritoBE objDistritosBE = new DistritoBE();
+                    objDistritosBE.idDistrito = objVendedor.idDistrito;
+                    objDistritosBE.nombreDistrito = objVendedor.nombreDistrito;
+                    objListaDistritos.Add(objDistritosBE);
+
+                }
+                return objListaDistritos;
+            }
+            catch (EntityException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+    }
+}
