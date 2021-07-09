efce7da2f8267125630d67ed008e4caedafda920
Diego Cáceres-Cardoza <diegokaiser@gmail.com>
2021-07-09 00:29:56 -0500

    login

--- www/Chambix/WEB_Chambix/App_Start/FilterConfig.cs
+++ www/Chambix/WEB_Chambix/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace WEB_Chambix
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new Filters.VerifySession());
         }
     }
 }
