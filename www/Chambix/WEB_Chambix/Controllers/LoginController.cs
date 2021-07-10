43dad9c7a85fe3b4045335fe0f0e1600e567cbc1
FiorellaRey <45338284+FiorellaRey@users.noreply.github.com>
2021-07-09 22:42:13 -0500

    controller&viewsUsuario

---
+++ www/Chambix/WEB_Chambix/Controllers/LoginController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WEB_Chambix.Models;
+
+namespace WEB_Chambix.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Enter(string emailUsuario, string contrasenaUsuario)
+        {
+            try
+            {
+                using(SistemaServiciosEntities db = new SistemaServiciosEntities())
+                {
+                    var lst = from d in db.tb_Usuario
+                              where d.emailUsuario == emailUsuario 
+                              && d.contrasenaUsuario == contrasenaUsuario 
+                              && d.estadoUsuario == true
+                              select d;
+                    if(lst.Count()>0)
+                    {
+                        tb_Usuario oUser = lst.First();
+                        Session["User"] = oUser;
+                        return Content("1");
+                    }
+                    else
+                    {
+                        return Content("Usuario inválido");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Content(":( no hay pan!" + ex.Message);
+            }
+
+        }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+    }
+}
--- www/Chambix/WEB_Chambix/Controllers/PostController.cs
+++ www/Chambix/WEB_Chambix/Controllers/PostController.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using WEB_Chambix.Models;
 
 namespace WEB_Chambix.Controllers
 {
     public class PostController : Controller
     {
+        private SistemaServiciosEntities db = new SistemaServiciosEntities();
+
         // GET: Post
         public ActionResult Index()
         {
-            return View();
+            var tb_Post = db.tb_Post.Include(t => t.tb_Distrito).Include(t => t.tb_SubCategoria).Include(t => t.tb_Usuario);
+            return View(tb_Post.ToList());
         }
     }
 }
